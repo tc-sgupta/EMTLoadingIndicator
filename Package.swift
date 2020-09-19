a62b6ec87586fea3a21655842efaa47d46022c99
Sourabh Gupta <sgupta@tigerconnect.com>
2020-09-20 11:03:35 +1200

    Try again dynamic again

--- Package.swift
+++ Package.swift
@@ -10,6 +10,7 @@ let package = Package(
     products: [
         .library(
             name: "EMTLoadingIndicator",
+            type: .dynamic,
             targets: ["EMTLoadingIndicator"])
     ],
     targets: [
