63f91d6369b9264549f331814c9376977e734998
Sourabh Gupta <sgupta@tigerconnect.com>
2020-09-20 09:07:05 +1200

    Added swift package manager support

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+import PackageDescription
+
+let package = Package(
+    name: "EMTLoadingIndicator",
+    platforms: [
+        .iOS(.v9)
+    ],
+    products: [
+        .library(
+            name: "EMTLoadingIndicator",
+            targets: ["EMTLoadingIndicator"])
+    ],
+    targets: [
+        .target(
+            name: "EMTLoadingIndicator",
+            path: ".",
+            sources: ["EMTLoadingIndicator/Classes"]),
+            resources: [.process("EMTLoadingIndicator/Resources")],
+    ],
+    swiftLanguageVersions: [.v5]
+)
